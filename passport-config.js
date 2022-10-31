fc229376576477cd91cf6a1bfddfc16241d8b131
Kasra Panahi <kasrapanahi2@gmail.com>
2022-10-30 22:56:28 -0700

    Version 1.0

---
+++ passport-config.js
@@ -0,0 +1,33 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async function(email, password, done) {
+        const user = getUserByEmail(email)
+
+        if(user == null) {
+            return done(null, false, {message: "No user with that email"})
+        }
+        
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, {message: "Password incorrect"})
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }
+
+    passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'}, authenticateUser))
+
+    passport.serializeUser((user, done) => {done(null, user.id)})
+
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
